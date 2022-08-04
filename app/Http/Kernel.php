2b5217c1f93b36423bd598d5e6e7c4d6f491fcec
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-08-04 11:47:07 +0200

    fixed a bug in kernel file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Http;
 
 use App\Http\Middleware\isAdminMiddleware;
 use App\Http\Middleware\isSuperAdminMiddleware;
+use App\Http\Middleware\isSuperAdminOrAdmin;
+use App\Http\Middleware\SuperAdminOrAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -67,6 +69,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => isAdminMiddleware::class,
         'is_superadmin' => isSuperAdminMiddleware::class,
-        'is_admin_or_superadmin' => isSuperAdminMiddleware::class
+        'is_admin_or_superadmin' => isSuperAdminOrAdmin::class
     ];
 }
--- app/Http/Middleware/isSuperAdminOrAdmin.php
+++ app/Http/Middleware/isSuperAdminOrAdmin.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class SuperAdminOrAdmin
+class isSuperAdminOrAdmin
 {
     /**
      * Handle an incoming request.
