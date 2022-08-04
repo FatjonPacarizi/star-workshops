1658a5ee9cf4cacedff3d8a690ab653286b809a0
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-08-04 14:24:17 +0200

    fixed inputs width

--- resources/views/appInfos.blade.php
+++ resources/views/appInfos.blade.php
@@ -3,15 +3,15 @@
   
       <div class="w-full bg-white border border-gray-200 rounded">
               <div class="w-full flex justify-between items-center  border-b border-gray-200 mb-4">
-              <h1 class = "p-3 text-slate-900">User Managment</h1>
+              <h1 class = "p-3 text-slate-900">App Information</h1>
               <a href="/dashboard" class="p-3 text-gray-400"> Cancel</a>
             </div>
-              <div class="flex flex-col items-center">
+              <div class="w-full p-4">
             <form method="POST" action="/appInfos/{{$informations[0]->id}}/edit" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="mb-6">
-                  <label for="company" class="inline-block text-lg mb-2">App Name</label>
+                  <label class="inline-block text-lg mb-2">App Name</label>
                   <input type="text" class="border border-gray-200 rounded p-2 w-full" name="app_name"
                     value="{{$informations[0]->app_name}}" />
           
@@ -22,7 +22,7 @@
           
             
                 <div class="mb-6">
-                  <label for="logo" class="inline-block text-lg mb-2">
+                  <label  class="inline-block text-lg mb-2">
                     App Logo
                   </label>
                   <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo_name" />
@@ -34,7 +34,7 @@
           
              
           
-                <div class="mb-6">
+                <div class="mb-6 flex justify-end">
                   <button class="bg-sky-500 text-white rounded py-2 px-4 hover:bg-black">
                     Update
                   </button>
