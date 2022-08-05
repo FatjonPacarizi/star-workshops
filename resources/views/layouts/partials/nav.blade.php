d3afaca466e2bed477855e9f03d4448734dfc445
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-08-05 11:24:28 +0200

    new-navbar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -48,21 +48,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="flex justify-end">
-                    @if (Route::has('login'))
-                    <div class="hidden  top-0 right-0 px-6 py-5 sm:block">
-                        @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                        <a href="{{ route('login') }}" class="p-2 rounded-full text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">Log in</a>
-
-                        @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="p-2 rounded-full text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">Register</a>
-                        @endif
-                        @endauth
-                    </div>
-                    @endif
-                </div>
             </div>
         </div>
 
