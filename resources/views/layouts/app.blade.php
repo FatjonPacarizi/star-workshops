e311a04281a2f87b16a32a6919bc86bdd4642b29
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-09 16:36:02 +0200

    fix active nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,14 @@
                             </a>
                         </li>
                         @endcan
+                        @can('is_super_admin')
+                        <li class="--set-active-tables-html my-2">
+                            <a href="{{ route('superadmin.showabouts') }}" class="flex p-2   rounded {{Request::is('abouts') ? 'bg-slate-500' : 'hover:bg-slate-600'}}">
+                                <span class="inline-flex items-center justify-center h-6 w-6"><i class="mdi mdi-account-outline inline-flex"></i></span>
+                                <span class="grow">About Us</span>
+                            </a>
+                        </li>
+                        @endcan
                         @can('is_admin_or_superadmin')
                         <li class="--set-active-tables-html my-2">
                             <a href="{{ route('adminsuperadmin.ShowAppInfos') }}" class="flex p-2   rounded {{Request::is('appInfos') ? 'bg-slate-500' : 'hover:bg-slate-600'}}">
