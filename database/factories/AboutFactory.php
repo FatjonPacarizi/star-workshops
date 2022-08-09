364810e960b99e1930c0f3dab423717e2401ea63
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-09 16:22:30 +0200

    Create AboutFactory.php

---
+++ database/factories/AboutFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AboutFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => 'About',
+            'heading' => 'We at StarLabs, know what it means to start from the bottom. We started from 0, and became 200!',
+            'paragraf' =>'StarLabs was established in 2015 in Prishtina, the Capital City of Kosovo in southeastern Europe. Our Company was established by two technology and business management experts who have over 10 years of experience in ICT outsourcing services. Located in 1700 square meters in the center of Prishtina, we operate with 200 engineering teams. Our biggest advantage is that we have access to the biggest pool of talents through our access in Digital School – The sister company, which is the biggest programming school for youth in the country as well as a franchise in over 50 locations worldwide.',
+            'image' => '1660054266.png',
+            'button' =>'https://www.starlabs.dev/',
+         
+        ];
+    }
+}
