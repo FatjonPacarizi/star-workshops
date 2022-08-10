5d20a04fe1c5783451f36a03f555893e3f26aa93
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-10 10:46:15 +0200

    delete button back
    delete button back in edit blade

--- resources/views/about/edit.blade.php
+++ resources/views/about/edit.blade.php
@@ -44,8 +44,8 @@
                             <img src="{{ asset('uploads/abouts/'.$about->image) }}" width="70px" height="70px" alt="Image">
                         </div>
                         <div class="w-full p-2 flex justify-end border-t border-gray-200">
-                            <button type="submit" class="rounded py-2 px-4 bg-blue-800 text-white hover:bg-blue-900">Update about</button>
-                            <a href="{{ url('abouts') }}" class="rounded py-2 px-5 bg-blue-800 text-white hover:bg-blue-900" style="margin: 2px 22px 2px;">BACK</a>
+                            <button type="submit" class="rounded py-2 px-4 bg-sky-500 text-white hover:bg-sky-600">Update about</button>
+
                   
                         </div>
 
