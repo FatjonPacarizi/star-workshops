cfad3cef1cfa65cad9ccb5f466f8e7008c2f7b91
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-08-12 15:31:29 +0200

    delete-old-logo-onupdate

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Informations;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\StoreInformationRequest;
 use App\Http\Requests\UpdateInformationRequest;
 
@@ -26,12 +27,28 @@ class InformationController extends Controller
         'app_name' => 'required',
     ]);
 
+
+    $appInfo =  Informations::find($id);
+
+
     if(request()->hasFile('logo_name')) {
         $formFields['logo_name'] = request()->file('logo_name')->store('logos','public');
+
+         //e ruajm old img para se me update
+         $oldImg = $appInfo->logo_name;
     }
 
+
+   //update appinfo
     Informations::find($id)->update($formFields);
 
+    
+    // delete old img only when db update is succesful
+    if(request()->hasFile('logo_name')) {
+        //delete old img
+        Storage::delete('/public/' .$oldImg);
+    }
+    
     return back();
 }
 }
