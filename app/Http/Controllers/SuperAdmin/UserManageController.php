968989a99632528e914531ce2bbe6379736f62a5
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-08-15 15:06:27 +0200

    fixing style on all workshops (style fix 5)

--- app/Http/Controllers/SuperAdmin/UserManageController.php
+++ app/Http/Controllers/SuperAdmin/UserManageController.php
@@ -13,7 +13,6 @@ class UserManageController extends Controller
      public function index(){
        
         return view('manageUsers',['users'=>User::all()]);
-        $users = DB::table('users')->count();
     }
 
     //Show edit form
