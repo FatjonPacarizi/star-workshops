625ab577b1850ea4e288fdc471640e01e06154b4
BlenardHasani <blenard0102@gmail.com>
2022-08-15 10:33:20 +0200

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -77,7 +77,7 @@
                 </aside>
             <div class="w-full flex justify-left items-left m-3">
                 <div class="p-6 flex h-fit bg-white w-full " >
-                    <div class="txt items-center border-r border-indigo-500">
+                    <div class="txt items-center border-r border-indigo-100">
                     <span class="inline-flex rounded-md">
                         <div class="p-6 max-w-sm bg-white ">
                             <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ Auth::user()->name }}<h1>
@@ -87,7 +87,7 @@
                     </span>
                     </div>
                     <div class="    ">
-                        <div class="flex text-center justify-around p-6 max-w-sm bg-white">
+                        <div class="flex text-center justify-around p-6  bg-white">
                         <div class="txt m-2 p-4 ">
                                 @php
                                 $workshops = App\Models\Workshop::find(1);
@@ -96,7 +96,7 @@
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Workshop</p>
                             
                         </div>
-                        <div class="txt border-r border-indigo-500">
+                        <div class="txt border-r border-indigo-100">
 
                         </div>
                         <div class="txt m-2 p-4 ">
@@ -109,7 +109,7 @@
                         
                         </div>
                         <div class="m-3  items-center">
-                            <div class="p-6 max-w-sm bg-white  ">
+                            <div class="p-6  bg-white  ">
                                 <img class="object-cover w-full" alt="Map" src="{{ asset('img/chart.png') }}">
                             </div>
                         </div>
