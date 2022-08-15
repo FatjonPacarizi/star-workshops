f3fde4d34ec455ef0cf8caeb7721286b0cde5eef
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-15 21:45:13 +0200

    store for image delete

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 use App\Models\About;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreAboutRequest;
+use App\Http\Requests\UpdateAboutRequest;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
@@ -33,20 +36,24 @@ class AboutController extends Controller
         $about->heading = $request->input('heading');
         $about->paragraf = $request->input('paragraf');
         $about->button = $request->input('button');
-        if($request->hasfile('image'))
-        {   
-            $destination = 'uploads/abouts/'.$about->image;
-           
-            if (File::exists($destination))
-            {
-                File::delete($destination);
-            }
-            $file = $request->file('image');
-            $extention = $file->getClientOriginalExtension();
-            $filetitle = time().'.'.$extention;
-            $file->move('uploads/abouts/', $filetitle);
-            $about->image = $filetitle;
-        }
+      
+    if(request()->hasFile('image')) {
+        $formFields['image'] = request()->file('image')->store('image','public');
+
+         //e ruajm old img para se me update
+         $oldImg = $about->image;
+    }
+
+
+   //update appinfo
+    Abouts::find($id)->update($formFields);
+
+    
+    // delete old img only when db update is succesful
+    if(request()->hasFile('image')) {
+        //delete old img
+        Storage::delete('/public/' .$oldImg);
+    }
      
         $about->update();
         return redirect()->back()->with('status','about Image Added Successfully');
