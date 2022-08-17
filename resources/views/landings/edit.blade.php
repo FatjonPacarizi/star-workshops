bfcc2e407542b506c4f7b604f3a86e97f2991159
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-08-17 13:30:43 +0200

    Update Landing

--- resources/views/landings/edit.blade.php
+++ resources/views/landings/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="w-full bg-white border border-gray-200 rounded mt-12">
         <div class="w-full flex justify-between items-center  border-b border-gray-200 mb-4">
 
-            <h1 class="p-3 text-slate-900">Landing Us</h1>
+            <h1 class="p-3 text-slate-900">Landing Page</h1>
             <a href="/landings" class="p-3 text-gray-400"> Cancel </a>
         </div>
         @if (session('status'))
@@ -36,11 +36,14 @@
                 <input type="text" name="button" value="{{$landing->button}}" class="border border-gray-200 rounded p-1 w-full mx-5">
             </div>
 
-            <div class="mb-6 flex items-center">
-                <label class="w-28 text-sm mx-5" for="">Image</label>
-                <input type="file" name="image" class="border border-gray-200 rounded p-1 w-full mx-5">
-                <img src="{{ asset('uploads/landings/'.$landing->image) }}" width="70px" height="70px" alt="Image">
+            <div class="mb-6  flex items-center">
+                <label class="w-28 text-sm mx-5">Image</label>
+                <input type="file" name="image" />
+
+                <img class="object-cover rounded" alt="Image" src="{{$landing->image ? asset('uploads/landings/'.$landing->image) : asset('/img/team1.png')}}" width="100">
+
             </div>
+
             <div class="w-full p-2 flex justify-end border-t border-gray-200">
                 <button type="submit" class="rounded py-2 px-4 bg-sky-500 text-white hover:bg-sky-600">Update landing</button>
 
--- resources/views/landings/index.blade.php
+++ resources/views/landings/index.blade.php
@@ -1,54 +1,64 @@
 @extends('layouts.app')
 @section('content')
-<div class="w-full h-full p-6  flex flex-col  items-center">
+<div class="w-full flex justify-left items-left overflow-y-auto mb-10">
+  <div class="w-full h-full p-6  flex flex-col  items-center ">
 
-  <div class="w-full bg-white border border-gray-200 rounded pb-4 mt-12">
+    <div class="w-full bg-white border border-gray-200 rounded pb-4 mt-12">
 
-    <h1 class="p-3 text-slate-900 border-b border-gray-200 mb-4 ">Landing Managment</h1>
-    <a class="mx-8" href="http://127.0.0.1:8000/add-landing">
-      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
-        </path>
-      </svg>
-    </a>
-    @if (session('status'))
-    <div class="w-full flex justify-center">{{ session('status') }}</div>
-    @endif
-    <div class="w-full flex justify-center">
-      <table class="w-full mx-4">
-        <tr class="border-y border-gray-200">
-          <td class="font-bold p-3">Page Title</td>
-          <td class="font-bold">Section Title</td>
-          <td class="font-bold">Description</td>
-          <td class="font-bold">Button Text</td>
-          <td class="font-bold">Image</td>
-          <td class="font-bold text-center w-1/9">Action</td>
-        </tr>
-        @foreach ($landing as $item)
-        <tr class='bg-gray-100'>
+      <div class="w-full flex justify-between items-center  border-b border-gray-200 mb-4">
+
+        <h1 class="p-3 text-slate-900">Landing Managment</h1>
+
+        <div class="flex mx-4 ">
+
+          <a class="mx-2 flex items-center" href="http://127.0.0.1:8000/add-landing">
+            <h6 class="text-2xl mr-1 -mt-1 text-gray-400 ">+</h6>
+            <h2 class="text-gray-400 ">Create Landing</h2>
+
+          </a>
+        </div>
+
+      </div>
 
 
-          <td>{{ $item->title }}</td>
+      @if (session('status'))
+      <div class="w-full flex justify-center">{{ session('status') }}</div>
+      @endif
+      <div class="w-full flex justify-center">
+        <table class="w-full mx-4">
+          <tr class="border-y border-gray-200">
+            <td class="font-bold p-3">Page Title</td>
+            <td class="font-bold">Section Title</td>
+            <td class="font-bold">Description</td>
+            <td class="font-bold">Button Text</td>
+            <td class="font-bold">Image</td>
+            <td class="font-bold text-center w-1/9">Action</td>
+          </tr>
+          @foreach ($landing as $item)
+          <tr class='bg-white-100 border-y border-gray-200'>
 
-          <td> {{ Illuminate\Support\Str::limit($item->heading, 20, $end='...') }}</td>
 
-          <td> {{ Illuminate\Support\Str::limit($item->paragraf, 20, $end='...') }}</td>
-          <td>{{ $item->button }}</td>
-          <td>
-            <img src="{{ asset('uploads/landings/'.$item->image) }}" width="70px" height="70px" alt="Image">
-          </td>
+            <td class="p-3">{{ $item->title }}</td>
 
-          <td>
-            <a href="{{ url('edit-landing/'.$item->id) }}" class="bg-sky-500 text-white px-4 py-1 text-sm rounded m-3">Edit</a>
-          </td>
+            <td> {{ Illuminate\Support\Str::limit($item->heading, 20, $end='...') }}</td>
 
-        </tr>
-        @endforeach
+            <td> {{ Illuminate\Support\Str::limit($item->paragraf, 20, $end='...') }}</td>
+            <td>{{ $item->button }}</td>
+            <td>
+              <img src="{{ asset('uploads/landings/'.$item->image) }}" width="70px" height="70px" alt="Image">
+            </td>
 
-      </table>
+            <td>
+              <a href="{{ url('edit-landing/'.$item->id) }}" class="bg-sky-500 text-white px-4 py-1 text-sm rounded m-3">Edit</a>
+            </td>
+
+          </tr>
+          @endforeach
+
+        </table>
+
+      </div>
 
     </div>
   </div>
-</div>
-
-@endsection
+  @endsection
