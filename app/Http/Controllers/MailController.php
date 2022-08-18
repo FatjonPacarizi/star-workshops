e8373e81ddb9b48d3b843b115e855325c2ebf417
Almir <almirpinduku@gmail.com>
2022-08-18 15:16:53 +0200

    Validation Contact-Form
    Validation Contact-Form

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,39 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MailRequest;
+use App\Models\About;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class MailController extends Controller
 {
-    
-    public function send(Request $request){
-        
-        $request->validate([
-         'name'=>'required',
-         'email'=>'required|email',
-         'subject'=>'required',
-         'message'=>'required'
-    ]);
-    if($this->isonline()){
-        $mail_data = [
-            'recipient'=>'mbereweb3@gmail.com',
-            'fromEmail'=>$request->email,
-            'fromName'=>$request->name,
-            'subject'=>$request->subject,
-            'body'=>$request->message
-        ];
-        \Mail::send('email-template',$mail_data,function($message)use($mail_data){
-            $message->to($mail_data['recipient'])
-                   ->from($mail_data["fromEmail"],$mail_data['fromName'])
-                   ->subject($mail_data['subject']);
-        });
-        return redirect()->back()->with('success','Email sent');
-    } else{
-        return redirect()->back()->withInput()->with('error','Check your internet connection');
-    }
-}
 
-    public function isOnline($site="https://youtube.com/"){
+        function send(MailRequest $request)
+        {
+
+            $request -> validate([
+                'name' => 'required',
+                'email' => 'required',
+                'subject' => 'required',
+            ]);
+
+            if ($this->isonline()) {
+                $mail_data = [
+                    'recipient' => 'startworkshop@gmail.com',
+                    'fromEmail' => $request->email,
+                    'fromName' => $request->name,
+                    'subject' => $request->subject,
+                    'body' => $request->message
+                ];
+                \Mail::send('email-template', $mail_data, function ($message) use ($mail_data) {
+                    $message->to($mail_data['recipient'])
+                        ->from($mail_data["fromEmail"], $mail_data['fromName'])
+                        ->subject($mail_data['subject']);
+                });
+                return redirect()->back()->with('success', 'Email sent');
+            } else {
+                return redirect()->back()->withInput()->with('error', 'Check your internet connection');
+            }
+        }
+
+    public function isOnline($site="https://www.starlabs.dev/"){
         if(@fopen($site, "r")){
             return true;
         }else{
