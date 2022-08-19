d7183181d1302fcbe7720df86ea94e5bfac7a677
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-19 15:38:49 +0200

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,10 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
+
     <body>
+
+   
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
