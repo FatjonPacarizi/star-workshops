43ea762eb38d78fd6a68fea5ea28e85ee4bfd19d
granitsalihu1 <granitsalihu9@gmail.com>
2022-08-24 14:33:00 +0200

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -175,6 +175,9 @@
         </div>
       @endforeach
     </div>
+
+    <button class="flex mx-auto mt-4 text-white bg-red-600 border-0 py-2 px-8 focus:outline-none rounded-full text-lg hover:bg-red-800 duration-300"><a href="http://127.0.0.1:8000/workshops" >View all events</a></button>
+
   </div>
 </section>
 
