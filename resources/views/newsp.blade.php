31f71937c92d80424353cb5207f86b8c64cf60b8
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-08-29 16:09:30 +0200

    Update

--- resources/views/newsp.blade.php
+++ resources/views/newsp.blade.php
@@ -7,9 +7,8 @@
     <img class="w-full mb-10 object-cover object-center rounded" alt="" src="{{$newspage->image_news ? asset('uploads/newspages/'.$newspage->image) : asset('/img/Workshops.jpg')}}">
     <div class="text-left lg:w-5/3 w-full">
       <h1 class="title-font sm:text-4xl text-4xl mb-4 font-medium text-black"><b>{{ $newspage->title }}</b></h1>
-      <h2 class="tracking-widest text-xl title-font font-medium text-gray-500 mb-1"><b>{{ $newspage->time }}</b></h2>
+      <h2 class="tracking-widest text-xl title-font font-medium text-gray-500 mb-1"><b>{{ $newspage->time }}</b> - <b>{{ $newspage->author }}</b></h2>
       <p class="mb-8 py-5 text-xl text-gray-700">{!! $newspage->description !!}</p>
-      <h2 class="tracking-widest text-base title-font font-medium text-gray-600 mb-1">{{ $newspage->author }}</h2>
     </div>
   </div>
 </section>
