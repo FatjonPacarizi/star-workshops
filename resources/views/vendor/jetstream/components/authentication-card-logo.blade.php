06c67656a80168b44eaa9872b9316eef2c951d36
Almir <almirpinduku@gmail.com>
2022-08-30 11:43:23 +0200

    Icon return home page

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,7 +1,6 @@
 <a href="/"> 
                         @php
                                $information = App\Models\Informations::all()->last();
-                        @endphp
-                        
+                        @endphp             
                             <img class="block h-20 p-5 w-auto" alt="Logo" src="{{$information->logo_name ? asset('/storage/' . $information->logo_name) : asset('/img/Logo.png')}}">
-                    </a>
+</a>
--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,5 +1,6 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+<div class="border border-red-700 w-full min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 relative flex justify-center">
+    <a href="/" class="absolute top-1/2 left-2.5 -mt-2"> <i class="fa-solid fa-house fa-xl text-gray-400"></i></a>
         {{ $logo }}
     </div>
 
