60d6a07ba606931ee0078fdef471cd629be4b365
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-09-01 11:57:29 +0200

    single-news design improve

--- resources/views/newsp.blade.php
+++ resources/views/newsp.blade.php
@@ -4,15 +4,15 @@
 
 <section class="text-black body-font flex flex-col items-center">
   <div class="w-11/12 flex flex-wrap">
-  <div class="w-full lg:w-3/4  lg:pr-10 ">
-    <img class="w-full h-1/4 border-4 border-red-500 my-10 object-cover object-center rounded " alt="" src="{{$newspage->image ? asset('/storage/' . $newspage->image) : asset('/img/defaultNewsImg.jpg')}}">
+  <div class="w-full  lg:w-3/4  lg:pr-10 ">
+    <img class="w-full lg:h-1/4 border-4 border-red-500 my-10 object-cover object-center rounded " alt="" src="{{$newspage->image ? asset('/storage/' . $newspage->image) : asset('/img/defaultNewsImg.jpg')}}">
     <div class="text-left lg:w-5/3 w-full">
       <h1 class="title-font sm:text-4xl text-4xl mb-4 font-medium text-black"><b>{{ $newspage->title }}</b></h1>
       <h2 class="tracking-widest text-xl title-font font-medium text-gray-500 mb-1"><b>{{ $newspage->time }}</b> - <b>{{ $newspage->author }}</b></h2>
       <p class="mb-8 py-5 text-xl text-gray-700">{!! $newspage->description !!}</p>
     </div>
   </div>
-  <div class="w-11/12 mx-auto mt-20 lg:mt-0 lg:-ml-10 md:w-2/3 lg:w-1/4  ">
+  <div class="w-11/12 mx-auto mt-10 lg:mt-0 lg:-ml-10 md:w-2/3 lg:w-1/4  ">
     @foreach($threenews as $newspage)
       <div class="mt-10 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img class="lg:h-48 md:h-36 w-full object-cover object-center" alt="" src="{{$newspage->image ? asset('/storage/' . $newspage->image) : asset('/img/defaultNewsImg.jpg')}}">
