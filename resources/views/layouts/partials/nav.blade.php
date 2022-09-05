e18b3088e02c4db1045d4bc3389dbdea87ec5caa
granitsalihu1 <granitsalihu9@gmail.com>
2022-09-05 15:49:00 +0200

    responsivenav

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -7,29 +7,22 @@
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
-                    <button type="button" class="sm:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                        <span class="sr-only">MENU</span>
-                        <!--
-            Icon when menu is closed.
-
-            Heroicon name: outline/menu
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                        <!--
-            Icon when menu is open.
-
-            Heroicon name: outline/x
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
+                    <div class="md:hidden flex items-center">
+	<button class="outline-none mobile-menu-button sm:hidden">
+		<svg
+			class="w-6 h-6 text-gray-500"
+			x-show="!showMenu"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+		<path d="M4 6h16M4 12h16M4 18h16"></path>
+		</svg>
+	</button>
+</div>
                 </div>
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex-shrink-0 flex items-center">
@@ -57,15 +50,25 @@
         </div>
 
         <!-- Mobile menu, show/hide based on menu state. -->
-        <div class="sm:hidden" id="mobile-menu">
-            <div class="px-2 pt-2 pb-3 space-y-1">
-                <!-- Current: "bg-red-500 text-white", Default: "text-white hover:bg-red-500 hover:text-white" -->
-                <a href="{{route('workshops')}}" class="text-red-700 hover:text-red-700 block px-3 py-2 rounded-md text-base font-medium">Workshops</a>
-                <a href="/newspage" class="text-red-700 hover:text-red-700 px-3 py-2 rounded-md text-sm font-medium">NEWS</a>
-                <a href="/members" class="text-red-700 hover:text-red-700 block px-3 py-2 rounded-md text-base font-medium">MEMBERS</a>
-                <a href="/about" class="text-red-700 hover:text-red-700 block px-3 py-2 rounded-md text-base font-medium">ABOUT US</a>
+        <div class="hidden mobile-menu">
+	<ul class="">
 
-            </div>
-        </div>
+        <li><a href="{{route('workshops')}}" class="text-red-700 hover:text-red-700 block text-sm px-2 py-4 transition duration-300" aria-current="page">WORKSHOPS</a>
+</li><li>    <a href="/newspage" class="text-red-700 hover:text-red-700 block text-sm px-2 py-4 transition duration-300">NEWS</a>
+</li><li>     <a href="/members" class="text-red-700 hover:text-red-700 block text-sm px-2 py-4 transition duration-300">MEMBERS</a>
+</li><li>     <a href="/about" class="text-red-700 hover:text-red-700 block text-sm px-2 py-4 transition duration-300">ABOUT US</a>
+</li>   
+    </ul>
+</div>
     </nav>
 </header>
+<script>
+	// Grab HTML Elements
+	const btn = document.querySelector("button.mobile-menu-button");
+	const menu = document.querySelector(".mobile-menu");
+
+	// Add Event Listeners
+	btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+	});
+</script>
