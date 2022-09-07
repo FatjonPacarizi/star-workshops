3db55c1e2956e9d536c5cb1caed20efdf82c4357
gramosshala <gramos_shala@live.com>
2022-09-07 23:32:00 +0300

    pagination added on workshops safedeletes

---
+++ app/Http/Livewire/Showsafeworkshops.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Workshop;
+use App\Models\User;
+use Livewire\WithPagination;
+use Illuminate\Database\Eloquent\Builder;
+
+class Showsafeworkshops extends Component
+{
+    use WithPagination;
+    protected $listeners = ['reloadSafeworkshops'];
+    public $search;
+    public $perpage;
+    public $sortby;
+
+    public function render(){
+
+        $page = 8;
+        if($this->perpage != null) $page =  $this->perpage;
+
+        $sort = "ASC";
+        if($this->sortby != null) $sort =  $this->sortby;
+
+        $workshops1 = Workshop::onlyTrashed()->whereHas('user', function (Builder $query) {
+            $query->where('name', 'like', '%'.$this->search.'%');
+        })
+        ->orwhere('name', 'like', '%'.$this->search.'%')
+        ->onlyTrashed()
+        ->orderBy('deleted_at',$sort)
+        ->paginate(8,['*'], 'deletedWorkshopsPage');
+
+        return view('livewire.showsafeworkshops',['workshops1'=> $workshops1]);
+    }
+
+    
+    public function reloadSafeworkshops($search,$perpage,$sortby){
+
+        $this->search = $search;
+        $this->perpage = $perpage;
+        $this->sortby = $sortby;
+    }
+}
