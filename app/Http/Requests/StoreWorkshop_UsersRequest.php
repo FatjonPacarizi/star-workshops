1d399d79aa0c1907a34b7af4076d968e46788ba2
gramosshala <gramos_shala@live.com>
2022-09-12 16:12:10 +0300

    admin can add participant

--- app/Http/Requests/StoreWorkshop_UsersRequest.php
+++ app/Http/Requests/StoreWorkshop_UsersRequest.php
@@ -13,7 +13,7 @@ class StoreWorkshop_UsersRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreWorkshop_UsersRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'workshop_id'=>'required',
+            'user_id'=>'required',
         ];
     }
 }
