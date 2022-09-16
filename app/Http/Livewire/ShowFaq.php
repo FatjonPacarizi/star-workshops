5f372ff12a24a9286f4049451b233a702ee0e4dd
BlenardHasani <102974619+BlenardHasani@users.noreply.github.com>
2022-09-16 10:35:31 +0200

    fixes (#258)
    * fixes
    
    * sort by status
    
    * fixes 2
    
    * Update filter-faq.blade.php
    
    * fixes3

--- app/Http/Livewire/ShowFaq.php
+++ app/Http/Livewire/ShowFaq.php
@@ -6,37 +6,29 @@ use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Faq;
 
+
 class ShowFaq extends Component
 {
-
     use WithPagination;
     protected $listeners = ['reloadFaq'];
     public $search;
     public $perpage;
     public $sortby;
 
-
     public function render()
     {
-
         $page = 8;
         if($this->perpage != null) $page =  $this->perpage;
 
         $sort = "ASC";
-        if($this->sortby != null) $sort =  $this->sortby;
-    
-
-        $faq = Faq::where('question','like','%'.$this->search.'%')
-        ->orderBy('id',$sort)
-        ->paginate($page);
-
-        return view('livewire.show-faq',['faq'=>$faq]);
+        if($this->sortby != null) $sort =  $this->sortby;       
+        return view('livewire.show-faq',['faq'=>Faq::where('question','like','%'.$this->search.'%')->orWhere('answer','like','%'.$this->search.'%')->orderBy('status',$sort)->paginate($page)]);
     }
-
-    public function reloadFaq($search,$perpage,$sortby){
+     public function reloadFaq($search,$perpage,$sortby){
 
         $this->search = $search;
-        $this->prepage = $perpage;
+        $this->perpage = $perpage;
         $this->sortby = $sortby;
-    }
+     }
 }
+    
