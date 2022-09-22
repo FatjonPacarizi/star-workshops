032312c51a335cea2e64439d604500bbe2a8cb56
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-09-22 10:52:08 +0200

    Fix PDF

--- resources/views/managePDF.blade.php
+++ resources/views/managePDF.blade.php
@@ -3,7 +3,8 @@ $date = new DateTime("now", new DateTimeZone('Europe/Tirane') );
 @endphp
 <div>
   <div>
-    <h1 style="text-align: center;">Workshop participants Managment</h1>
+    <h1 style="text-align: center;">{{$workshopName[0]->name}}</h1>
+    <h3 class="text-align: center;">Workshop participants Managment</h3>
   </div>
   <div>
     <p style="font-family: Arial, Helvetica, sans-serif; color:orange;">Pending</p>
@@ -23,34 +24,7 @@ $date = new DateTime("now", new DateTimeZone('Europe/Tirane') );
     </table>
     @if(count($pendingParticipants) == 0) <p style="text-align: center;"> No pending participant found</p> @endif
     <div>
-<<<<<<< HEAD
-      <h1 style="text-align: center;">{{$workshopName[0]->name}}</h1>
-      <h3 class ="text-align: center;">Workshop participants Managment</h3>
-    </div>
-    <div>
-      <p style="font-family: Arial, Helvetica, sans-serif; color:orange;">Pending</p>
-      <table style="font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;">
-        <tr style="padding: 8px; text-align: left; border-bottom: 1px solid #ddd; color: gray;">
-          <td>User Name</td>
-          <td>User Email</td>
-          <td>Applied On</td>
-        </tr>
-
-        @foreach($pendingParticipants as $pendingParticipant)
-        <tr style="padding: 8px; text-align: left; border-bottom: 1px solid #ddd;">
-          <td>{{$pendingParticipant->user->name}}</td>
-          <td>{{$pendingParticipant->user->email}}</td>
-          <td><a href="#"> {{$pendingParticipant->created_at}}</a></td>
-        </tr>
-        @endforeach
-      </table>
-      @if(count($pendingParticipants) == 0) <p style="text-align: center;"> No pending participant found</p> @endif
-      <div>
-        {{ $pendingParticipants->links() }}
-      </div>
-=======
       {{ $pendingParticipants->links() }}
->>>>>>> 0db32f5439fb71e8b1b10c2c093308e70d685c72
     </div>
   </div>
 
