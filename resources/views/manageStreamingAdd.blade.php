5799ad2d4052a3a03f97cfae2b0fcb676af8dbc4
gramosshala <77172595+gramosshala@users.noreply.github.com>
2022-09-22 11:26:29 +0300

    Update manageStreamingAdd.blade.php

--- resources/views/manageStreamingAdd.blade.php
+++ resources/views/manageStreamingAdd.blade.php
@@ -3,7 +3,7 @@
 <div class="w-full h-full px-10 flex flex-col  items-center">
   <div class="w-full flex justify-between items-center">
     <a href="" class = " bg-white w-8 h-8 rounded-lg flex items-center justify-center border border-gray-200 text-gray-400 hover:shadow  "><i class="fa-solid fa-plus font-thin text-2xl"></i></a>
-    <livewire:filter-faq/>
+    
 </div>
   <div class="w-full bg-white shadow-md rounded-xl  py-4 " >
 
