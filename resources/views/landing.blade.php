baa7b68310cc9c99ef3cc1e6fdd3625172225f38
Almir <almirpinduku@gmail.com>
2022-09-27 14:00:43 +0200

    smallfix

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -94,7 +94,7 @@
           <div class="p-6 ">
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $newspage->title }}</h1>
             <p class="leading-relaxed text-gray-700 mb-3">{!! Illuminate\Support\Str::limit($newspage->description, 50, $end='...') !!}</p>
-            <h2 class="tracking-widest text-3xs title-font font-medium text-gray-500 mb-1">{{ $newspage->time }} - {{ $newspage->author }}</h2>
+            <h2 class="tracking-widest text-3xs title-font font-medium text-gray-500 mb-1">{{ \Carbon\Carbon::parse($newspage->time)->format('d/m/Y')}}</h2>
             <div class="flex items-center flex-wrap ">
               <a class="text-red-600 inline-flex items-center md:mb-2 lg:mb-0" href="{{route('single-news',$newspage->id)}}">Read More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
