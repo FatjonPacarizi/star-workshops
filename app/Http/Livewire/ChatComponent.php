89c82ccb2a453dde2b9ce5273cc62bd457d78c4e
arditshaqiri <109625917+arditshaqiri@users.noreply.github.com>
2022-09-30 22:08:15 +0200

    Feature/websocket chat (#306)
    * added message time, code refactor etc..
    
    * update
    
    * typing dots

--- app/Http/Livewire/ChatComponent.php
+++ app/Http/Livewire/ChatComponent.php
@@ -8,35 +8,28 @@ use Livewire\Component;
 
 class ChatComponent extends Component
 {
-    public $message;
     public $workshop;
 
     public function render()
     {
         return view('livewire.chat-component', ['messages' => Message::where('workshop_id',$this->workshop->id)->get(),'workshop_id'=>$this->workshop->id]);
     }
-    public function send()
+    public function send($msg)
     {
-        if($this->message != null){
+        if($msg != null && $msg != ''){
 
-            $array = array();
-            $array['name'] = auth()->user()->name;
-            $array['message'] = $this->message;
-            $array['status'] = auth()->user()->user_status;
+            event(new \App\Events\MessageEvent($msg,auth()->user()->name,auth()->user()->user_status,substr(\Carbon\Carbon::now('Europe/Tirane'),11,-3),false,$this->workshop->id));
 
-            //dd(json_encode($array));
-            event(new \App\Events\MessageEvent($array,$this->workshop->id));
-
-            $msg =  $this->message;
-
-            $this->message = null;
-
-            Message::insert([
+            Message::create([
                 'sender_id' => auth()->user()->id,
                 'workshop_id' => $this->workshop->id,
                 'message' => $msg
             ]
             );
         }
+    }
+    public function typing()
+    {
+            event(new \App\Events\MessageEvent(null,null,null,null,true,$this->workshop->id));
     }
 }
