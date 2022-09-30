3090175a3a348e40478a64f01abd9f3dd135db87
granitsalihu1 <granitsalihu9@gmail.com>
2022-09-30 14:06:26 +0200

    Update single-member.blade.php

--- resources/views/single-member.blade.php
+++ resources/views/single-member.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.landinglayouts')
 @section('content')
-
-<section class="text-black body-font flex flex-col items-center">
-  <div class="w-4/5 flex flex-wrap">
-    <div class="w-full  lg:w-3/4  lg:pr-10 ">
-    <img class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 w-14" 
-                src="{{$staffMembers->profile_photo_path ? asset('/storage/' . $staffMembers->profile_photo_path) : asset('img/defaultuserphoto.png') }}"
-                alt="" />
-        <div class="text-left lg:w-5/3 w-full">
-          <h1 class="title-font sm:text-4xl text-4xl mb-4 font-medium text-black"><b>{{ $staffMembers->title }}</b></h1>
-          <p class="mb-8 py-5 text-xl text-gray-700">{!! $staffMembers->description !!}</p>
-        </div>
-        <div class="flex h-10 mt-5 ">
-                <a href="{{$staffMembers->instagram}}" target="_blank"><i class="fa-brands fa-instagram fa-2xl"
+<section class="w-full text-white body-font">
+  <div class=" w-full flex flex-wrap  py-24 justify-between items-center ">
+    <div class=" md:w-1/2 lg:px-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center text-black ">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium ">{{ $staffMembers->name }}
+      </h1>
+      <p class="mb-8 leading-relaxed">{!! $staffMembers->email !!}</p>
+      <p class="mb-8 leading-relaxed">{!! $staffMembers->description !!}</p>
+      <div class="flex justify-center">
+      <div class="flex h-10 mt-5 ">
+      <a class="" href="{{$staffMembers->instagram}}" target="_blank"><i class="fa-brands fa-instagram fa-2xl"
                         style="color:red"></i></a>
                 <a href="{{$staffMembers->facebook}}" target="_blank"><i class="fa-brands fa-facebook fa-2xl mx-4"
                         style="color:red"></i></a>
                 <a href="{{$staffMembers->github}}" target="_blank"><i class="fa-brands fa-github fa-2xl"
-                        style="color:red"></i></a>
+                        style="color:red"></i></a>  
             </div>
-    </div>
-    
- 
 </div>
-  <a href="/members" class="tracking-widest text-xl title-font font-medium text-gray-500 mb-4">Show more </a>
+    </div>
+    <div class="w-2/4 md:w-1/2 border relative  mt-10 md:mt-0 py-10  rounded-l-3xl bg-gray-200 md:pl-16 flex text-center">
+    
+      <div class="absolute w-2/4 rounded-l-3xl h-full right-0 top-0  bg-red-600 "></div>
+      <img class="w-2/4 object-cover object-center  rounded-l-3xl z-10" alt="Workshops and Training"  src="{{$staffMembers->profile_photo_path ? asset('/storage/' . $staffMembers->profile_photo_path) : asset('img/defaultuserphoto.png') }}">
+    </div>
+  </div>
+ 
 </section>
+
+
 @endsection
