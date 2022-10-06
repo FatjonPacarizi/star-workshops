7c1b8ac4a6630f129ef426c3298c7b4a80bd1117
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-10-06 14:59:50 +0200

    Calendar fix

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Asantibanez\LivewireCalendar\LivewireCalendarServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -232,6 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        "LivewireCalendar" => \Asantibanez\LivewireCalendar\LivewireCalendarFacade::class,
 
     ],
 
