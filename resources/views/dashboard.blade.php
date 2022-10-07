5042bdab6382624c26d61e771b3c4a0262f55631
granitsalihu1 <granitsalihu9@gmail.com>
2022-10-07 10:40:31 +0200

    insert a new chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,66 +11,50 @@
             </span>
         </div>
         <div class="txt m-2 p-4 ">
-            @php
+        <div class="container p-5">     @php
             $users = App\Models\User::find(1);
             @endphp
             <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{$users->count()}} </h1>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Users</p>
+            @php
+            $workshops = App\Models\Workshop::find(1);
+            @endphp
+            <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{$workshops->count()}} </h1>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Workhops</p>
+    
+ 
+        <div  id="google-line-chart" style="width: 900px; height: 500px"></div>
+ 
+    </div>
         </div>
-        <div class="">
-            <div class="flex text-center justify-around p-6  bg-white" id="chart">
-                <div class="txt border-r border-indigo-100">
-                </div>
-            </div>
-        </div>
-        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
-            var workshops = <?php echo json_encode($workshops)?>;
-    Highcharts.chart('chart', {
-        title: {
-            text: 'New Workshops 2022'
-        },
-      
-        xAxis: {
-            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-                'October', 'November', 'December'
-            ]
-        },
-        yAxis: {
-            title: {
-                text: 'Number of New Workshops'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-        plotOptions: {
-            series: {
-                allowPointSelect: true
-            }
-        },
-        series: [{
-            name: 'New Workshop',
-            data: workshops
-        }],
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+ 
+        function drawChart() {
+ 
+        var data = google.visualization.arrayToDataTable([
+            ['Month Name', 'Register Workshop Count'],
+ 
+                @php
+                foreach($lineChart as $d) {
+                    echo "['".$d->month_name."', ".$d->count."],";
                 }
-            }]
+                @endphp
+        ]);
+ 
+        var options = {
+          title: 'Register Workshops Month ',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+ 
+          var chart = new google.visualization.LineChart(document.getElementById('google-line-chart'));
+ 
+          chart.draw(data, options);
         }
-    });
-        </script>
+    </script>
     </div>
 </div>
 
