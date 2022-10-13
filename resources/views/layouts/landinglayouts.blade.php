097c03c4e249e77df8072aea2c7c56840ff09b46
Ardit Shaqiri <arditshaqiri97@gmail.com>
2022-10-13 14:07:32 +0200

    update

--- resources/views/layouts/landinglayouts.blade.php
+++ resources/views/layouts/landinglayouts.blade.php
@@ -20,8 +20,7 @@
     <link rel="stylesheet" href="{{ asset('css/vanillacss.css') }}">
 
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="  crossorigin="anonymous"></script>
 
